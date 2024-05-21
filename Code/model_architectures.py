3ba8d25a974b70e46648d5a4294824364f12014b
Philip Meixner <philip.meixner@gmail.com>
2024-05-21 14:42:59 +0200

    testing

--- Code/model_architectures.py
+++ Code/model_architectures.py
@@ -33,6 +33,70 @@ class EnhancedAudioCNN(nn.Module):
         return x
 
 
+class EnhancedAudioCNN110(nn.Module):
+    def __init__(self):
+        super(EnhancedAudioCNN110, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=1)
+        self.bn1 = nn.BatchNorm2d(32, momentum=0.01)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1)
+        self.bn2 = nn.BatchNorm2d(64, momentum=0.1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(0.1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        self.bn3 = nn.BatchNorm2d(128, momentum=0.1)
+        self.conv4 = nn.Conv2d(128, 64, kernel_size=(3, 3), padding=1)
+        self.bn4 = nn.BatchNorm2d(64, momentum=0.1)
+        self.fc1 = nn.Linear(64 * 6 * 2, 32)
+        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.1)
+        self.fc2 = nn.Linear(32, 20)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.dropout1(x)
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = self.pool(self.relu(self.bn4(self.conv4(x))))
+        x = self.dropout2(x)
+        x = x.view(-1, 64 * 6 * 2)  # Adjust the flattening to new layer dimensions
+        x = self.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+
+
+class EnhancedAudioCNN15(nn.Module):
+    def __init__(self):
+        super(EnhancedAudioCNN15, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=1)
+        self.bn1 = nn.BatchNorm2d(32, momentum=0.01)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1)
+        self.bn2 = nn.BatchNorm2d(64, momentum=0.1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout1 = nn.Dropout(0.1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        self.bn3 = nn.BatchNorm2d(128, momentum=0.1)
+        self.conv4 = nn.Conv2d(128, 64, kernel_size=(3, 3), padding=1)
+        self.bn4 = nn.BatchNorm2d(64, momentum=0.1)
+        self.fc1 = nn.Linear(64 * 1 * 5, 32)
+        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.1)
+        self.fc2 = nn.Linear(32, 20)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.dropout1(x)
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = self.relu(self.bn4(self.conv4(x)))
+        x = self.dropout2(x)
+        x = x.view(-1, 64 * 1 * 5)  # Adjust the flattening to new layer dimensions
+        x = self.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        return x
+
+
 #  PAST MODELS
 
 class AudioCNN(nn.Module):
